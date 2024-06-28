b270338df7fcbad4af7339efb83b93169ebed50a
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-06-28 20:07:28 +0600

    create api for particular user for getting book

--- api/controller/books.controller.js
+++ api/controller/books.controller.js
@@ -25,3 +25,46 @@ export const createBooks = async (req, res) => {
     });
   }
 };
+
+export const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      message: "Books fetched successfully",
+      success: true,
+      books: books,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+      error: error,
+    });
+  }
+};
+
+export const getBooksForUser = async (req, res) => {
+  if (req.user.id === req.params.userId) {
+    try {
+      const books = await Book.find({ bookOwner: req.params.userId });
+      return res.status(200).json({
+        message: "Books fetched successfully",
+        success: true,
+        books: books,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Internal Server Error",
+        success: false,
+        error: error,
+      });
+    }
+  } else {
+    return res.status(403).json({
+      message: "You are not allowed to get books for this user",
+      success: false,
+      error: error,
+    });
+  }
+};
--- api/routes/books.routes.js
+++ api/routes/books.routes.js
@@ -1,7 +1,13 @@
 import express from "express";
 import { verifyToken } from "../../helper/verifytoken.js";
-import { createBooks } from "../controller/books.controller.js";
+import {
+  createBooks,
+  getAllBooks,
+  getBooksForUser,
+} from "../controller/books.controller.js";
 
 const router = express.Router();
 router.post("/create-book", verifyToken, createBooks);
+router.get("/get-all-books", getAllBooks);
+router.get("/get-all-books/:userId", verifyToken, getBooksForUser);
 export default router;
